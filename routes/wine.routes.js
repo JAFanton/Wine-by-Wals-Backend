b3de67680256a9dee43db12af5fae5193415385d
JAFanton <justin.fanton@gmail.com>
2025-09-03 14:34:48 +0200

    feat: add Wine routes with GET, POST, PUT, DELETE using promise syntax and authentication

---
+++ routes/wine.routes.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = express.Router();
+const Wine = require("../models/Wine.model");
+const { isAuthenticated } = require("../middleware/jwt.middleware");
+
+//GET request for /wines, for all Wines (public)
+router.get("/", (req, res) => {
+    Wine.find()
+    .then((wines) => res.status(200).json({ data: wines}))
+    .catch((error) => res.status(500).json({ error: "Failed to fetch wines", details: error })
+);
+});
+
+//POST request to /wines, add new wines to the API (Authenticated users only)
+router.post("/", isAuthenticated, (req, res) => {
+    Wine.create(req.body)
+    .then((newWine) => res.status(201).json({ data: newWine}))
+    .catch((error) => res.status(400).json({ error: "Failed to add new wine"})
+);
+});
+
+//PUT request to /Wines/:id, Update a wine by ID (Authenticated users only)
+router.put("/:id", isAuthenticated, (req, res) => {
+    Wine.findByIdAndUpdate(req.params.id, req.body, { new:true })
+    .then((updatedWine) => {
+        if(!updatedWine) return res.status(404).json({ error: "Wine not found" });
+        res.status(200).json({ data: updatedWine });
+    })
+    .catch((error) => {
+        res.status(400).json({ error: "Failed to update Wine", details: error })
+    });
+});
+
+// DELETE request to /wines/:id, deletes a wine by ID (Authenticated users only)
+router.delete("/:id", isAuthenticated, (req, res) => {
+    Wine.findByIdAndDelete(req.params.id)
+    .then((deletedWine) => {
+        if (!deletedWine) return res.status(404).json({ error: "Wine not found "});
+        res.status(200).json({ message: "Wine has been successfully deleted "});
+    })
+    .catch((error) => {
+        res.status(400).json({ error: "failed to delete wine", details: error })
+    });
+});
+
+module.exports = router;
