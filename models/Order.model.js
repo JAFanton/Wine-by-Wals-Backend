b5736663ebf3645eb1f1b29bf3c6434990a2e64d
JAFanton <justin.fanton@gmail.com>
2025-09-04 11:06:10 +0200

    feat: add Order model for storing completed user orders

---
+++ models/Order.model.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+
+const orderSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+    required: "true",
+  },
+  items: [
+    {
+      wine: {
+        type: Schema.Types.ObjectId,
+        ref: "wine",
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+        min: 1,
+      },
+    },
+  ],
+  totalPrice: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "processed", "shipped"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
+
+const Order = model("order", orderSchema);
+
+module.exports = Order;
