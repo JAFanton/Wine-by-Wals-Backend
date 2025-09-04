2c46904954b166ef7b3864acef0db7766a8f8ac6
JAFanton <justin.fanton@gmail.com>
2025-09-04 13:09:24 +0200

    chore: register temporary admin route in app.js for testing

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ app.use("/cart", cartRoutes)
 const orderRoutes = require("./routes/order.routes");
 app.use(".orders", orderRoutes);
 
+const adminRoutes = require("./routes/admin.routes");
+app.use("/admin", adminRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
